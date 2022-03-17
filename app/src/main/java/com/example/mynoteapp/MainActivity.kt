6b35da5625021f3cd5f75a87057963b38129dce3
jurgielewicz <patrykjurgielewicz123@gmail.com>
2022-03-17 19:27:07 +0100

    database

--- app/src/main/java/com/example/mynoteapp/MainActivity.kt
+++ app/src/main/java/com/example/mynoteapp/MainActivity.kt
@@ -8,36 +8,22 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.mynoteapp.ui.theme.MyNoteAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@ExperimentalComposeUiApi
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MyNoteAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    MyNoteAppTheme {
-        Greeting("Android")
-    }
-}
---
+++ app/src/main/java/com/example/mynoteapp/data/NoteDatabaseDao.kt
@@ -0,0 +1,26 @@
+package com.example.mynoteapp.data
+
+import androidx.room.*
+import com.example.mynoteapp.model.Note
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface NoteDatabaseDao {
+    @Query("SELECT * from notes_table")
+    fun getNotes():
+            Flow<List<Note>>
+    @Query("SELECT * from notes_table where id =:id")
+    suspend fun getNoteById(id: String): Note
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(note: Note)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun update(note: Note)
+
+    @Query("DELETE from notes_table")
+    suspend fun deleteAll()
+
+    @Delete
+    suspend fun deleteNote(note: Note)
+}
